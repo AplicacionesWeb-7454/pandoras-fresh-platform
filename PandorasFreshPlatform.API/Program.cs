bd253255c4e5e78b71321a3d5891d66532a8632e
DiegoLlamozas <diegollamozas911@gmail.com>
2025-10-30 15:50:05 -0500

    feat(app): set up Program.cs with core services and configurations.

--- PandorasFreshPlatform.API/Program.cs
+++ PandorasFreshPlatform.API/Program.cs
@@ -1,8 +1,44 @@
+using PandorasFreshPlatform.API.Shared.Domain.Repositories;
+using PandorasFreshPlatform.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using PandorasFreshPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using PandorasFreshPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
+
+// Add Database Connection
+if (builder.Environment.IsDevelopment())
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            if (string.IsNullOrEmpty(connectionString)) throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            
+            var password = builder.Configuration["DbPassword"];
+            if (string.IsNullOrEmpty(password))
+                throw new InvalidOperationException("Database password not found in user secrets.");
+            
+            // Build the complete connection string
+            var completeConnectionString = $"{connectionString}password={password};";
+            
+            options.UseMySQL(completeConnectionString)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        });
+
+// Bounded Contexts Dependency Injection
+// Shared Context Dependency Injection
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// News Context Dependency Injection
+
 
 var app = builder.Build();
 
@@ -10,32 +46,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+using var scope = app.Services.CreateScope();
+scope.ServiceProvider.GetRequiredService<AppDbContext>().Database.EnsureCreated();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}

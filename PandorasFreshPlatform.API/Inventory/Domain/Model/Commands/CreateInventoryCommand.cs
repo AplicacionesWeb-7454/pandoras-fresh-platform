c1a69308b2c9d8fcbf911b01dbd759c894b63c58
DiegoLlamozas <diegollamozas911@gmail.com>
2025-10-31 18:59:47 -0500

    feat(commands): add inventory management commands.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/CreateInventoryCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+/// <summary>
+///     Command to create an Inventory .
+/// </summary>
+/// <param name="ClientId">
+///     The Id of the Client for the Inventory
+/// </param>
+///
+/// <param name="Name">
+///     The name of the inventory to create.
+/// </param>
+
+public record CreateInventoryCommand(int ClientId, string Name);

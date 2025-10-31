1da2fa99de173caa29ede5b5510f75df14f93dd0
DiegoLlamozas <diegollamozas911@gmail.com>
2025-10-31 17:43:02 -0500

    feat(domain): add ProductIdentifier value object.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Entities/ProductIdentifier.cs
@@ -0,0 +1,8 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Entities;
+
+public record ProductIdentifier(Guid Identifier)
+{
+    public ProductIdentifier() : this(Guid.NewGuid())
+    {
+    }
+}

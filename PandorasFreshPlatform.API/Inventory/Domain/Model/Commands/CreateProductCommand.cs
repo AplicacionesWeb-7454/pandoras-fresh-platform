37ca211429a973b95501e5a877e74cdde2604b46
DiegoLlamozas <diegollamozas911@gmail.com>
2025-10-31 19:00:49 -0500

    feat(commands): add inventory management commands

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/CreateProductCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+using PandorasFreshPlatform.API.Inventory.Domain.Model.Entities;
+
+/// <summary>
+///     Command to create an Inventory .
+/// </summary>
+/// <param name="ProductId">
+///     The Id of the Product 
+/// </param>
+///
+/// <param name="Name">
+///     The name of the Product to Add to 
+/// </param>
+///
+/// <param name="CategoryId">
+///     The Id of the Category for the product
+/// </param>
+/// 
+/// 
+
+public record CreateProductCommand(ProductIdentifier ProductId, string Name, int CategoryId);
---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/CreateStorageBoxCommand.cs
@@ -0,0 +1,25 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+/// <summary>
+///     Command to create an Inventory .
+/// </summary>
+/// <param name="StorageBoxId">
+///     The Id of the Storage Box 
+/// </param>
+///
+/// <param name="Name">
+///     The name of the Storage Box 
+/// </param>
+///
+/// <param name="BoxType">
+///     The Type of the Storage Box
+/// </param>
+///
+///<param name="SensorId"> 
+///     The Id of the Sensor of the Storage Box
+/// </param> 
+/// 
+
+public record CreateStorageBoxCommand(int StorageBoxId, string Name, BoxType BoxType, string? SensorId = null); 

07577522734e56dace71974162f461707ca6da69
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:35:12 -0500

    feat(queries): add get expiring products query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetExpiringProductsQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get expiring products in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="DaysUntilExpiration">
+/// The number of days until expiration to filter by
+/// </param>
+public record GetExpiringProductsQuery(int DaysUntilExpiration);

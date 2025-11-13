4ccbff1703e4fc317c576d2c46d449349c402b66
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:45:15 -0500

    feat(domain): add Location value object.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/ValueObjects/Location.cs
@@ -0,0 +1,6 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents a location in the Pandora's Fresh Platform
+/// </summary>
+public record Location(string Value);

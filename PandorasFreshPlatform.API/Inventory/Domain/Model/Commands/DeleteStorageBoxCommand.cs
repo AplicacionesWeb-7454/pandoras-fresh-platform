9c81cd776fcbba46ff2896119ef0aed01f34f94d
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:19:12 -0500

    feat(storage): add command to delete storage boxes.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/DeleteStorageBoxCommand.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+/// <summary>
+/// Command to delete a storage box.
+/// </summary>
+/// <param name="StorageBoxId">
+/// The ID of the storage box to delete.
+/// </param>
+public record DeleteStorageBoxCommand(StorageBoxId StorageBoxId);

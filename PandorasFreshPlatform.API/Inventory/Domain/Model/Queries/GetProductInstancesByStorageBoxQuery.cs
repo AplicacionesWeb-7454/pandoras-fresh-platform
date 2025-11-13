21ca74d9e88bb97b60b9f92e96048e05b709be18
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:38:50 -0500

    feat(queries): add get product instances by storage box query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetProductInstancesByStorageBoxQuery.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get product instances by storage box in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="StorageBoxId">
+/// The id of the storage box to get product instances for
+/// </param>
+public record GetProductInstancesByStorageBoxQuery(StorageBoxId StorageBoxId);

a3d8c65f4ffc1bd6555daa14a7688c24fa9aafba
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:58:46 -0500

    feat(inventory): add get all inventories query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetAllInventoriesQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get all inventories in the Pandora's Fresh Platform.
+/// </summary>
+public record GetAllInventoriesQuery;

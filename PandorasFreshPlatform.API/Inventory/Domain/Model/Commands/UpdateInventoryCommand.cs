6ba77b5c572233ec734cc7fb3225a9d355a27da5
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:20:54 -0500

    feat(inventory): add command to update inventory information.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/UpdateInventoryCommand.cs
@@ -0,0 +1,25 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+/// <summary>
+/// Command to update an inventory.
+/// </summary>
+/// <param name="InventoryId">
+/// The ID of the inventory to update.
+/// </param>
+/// <param name="Name">
+/// The updated name of the inventory.
+/// </param>
+/// <param name="Description">
+/// The updated description of the inventory.
+/// </param>
+/// <param name="Location">
+/// The updated location of the inventory.
+/// </param>
+public record UpdateInventoryCommand(
+    InventoryItemId InventoryId, 
+    string Name, 
+    string Description, 
+    string Location
+);

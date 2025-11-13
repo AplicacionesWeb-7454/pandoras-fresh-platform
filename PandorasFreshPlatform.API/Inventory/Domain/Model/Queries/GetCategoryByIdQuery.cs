9efa4182e7d512311e71c12dd1f9b248d9c92d54
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:34:50 -0500

    feat(queries): add get category by id query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetCategoryByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get a category by id in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="CategoryId">
+/// The id of the category to get
+/// </param>
+public record GetCategoryByIdQuery(CategoryId CategoryId);

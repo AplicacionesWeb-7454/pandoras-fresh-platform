f38602b4f095a1c036274c3d7f4bd5de1914c7de
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:41:00 -0500

    feat(queries): add search products by term query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/SearchProductsQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to search products in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="SearchTerm">
+/// The term to search for in product names and descriptions
+/// </param>
+public record SearchProductsQuery(string SearchTerm);

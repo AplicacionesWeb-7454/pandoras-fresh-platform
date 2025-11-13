4ee329fdbcb156fc22681f3609dd6e8312c5e3f1
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:13:14 -0500

    feat(api): add ScanBarcodeResource for barcode scanning requests.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Resources/ScanBarcodeResource.cs
@@ -0,0 +1,12 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+
+/// <summary>
+/// Resource to scan a barcode
+/// </summary>
+/// <param name="Barcode">
+/// The barcode to scan
+/// </param>
+/// <param name="DeviceId">
+/// The ID of the device performing the scan
+/// </param>
+public record ScanBarcodeResource(string Barcode, string DeviceId);

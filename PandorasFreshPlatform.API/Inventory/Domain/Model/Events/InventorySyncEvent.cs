a38dde092003ce17ea2455e924263eb0756c93d2
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:28:01 -0500

    feat(events): add inventory synchronization event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/InventorySyncEvent.cs
@@ -0,0 +1,10 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class InventorySyncEvent(string deviceId, DateTime syncTime, int itemsSynced) : IEvent
+{
+    public string DeviceId { get; } = deviceId;
+    public DateTime SyncTime { get; } = syncTime;
+    public int ItemsSynced { get; } = itemsSynced;
+}

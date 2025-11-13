e357a1b223580d95212e329ae91eaf86e66da7be
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:26:18 -0500

    feat(events): add barcode scanned domain event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/BarcodeScannedEvent.cs
@@ -0,0 +1,10 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class BarcodeScannedEvent(string barcode, string deviceId, DateTime scanTime) : IEvent
+{
+    public string Barcode { get; } = barcode;
+    public string DeviceId { get; } = deviceId;
+    public DateTime ScanTime { get; } = scanTime;
+}

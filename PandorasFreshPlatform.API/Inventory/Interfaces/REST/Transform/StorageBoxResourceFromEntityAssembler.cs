1c4c4c7b7d7cbeecadd8c8eecc9636549ba8c778
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:22:11 -0500

    feat(api): add assembler for storage box entity to resource conversion.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Transform/StorageBoxResourceFromEntityAssembler.cs
@@ -0,0 +1,30 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.Entities;
+using PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+
+namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler class to convert StorageBox to StorageBoxResource
+/// </summary>
+public static class StorageBoxResourceFromEntityAssembler
+{
+    /// <summary>
+    /// Convert StorageBox to StorageBoxResource
+    /// </summary>
+    /// <param name="entity">
+    /// The <see cref="StorageBox" /> entity
+    /// </param>
+    /// <returns>
+    /// The <see cref="StorageBoxResource" /> resource
+    /// </returns>
+    public static StorageBoxResource ToResourceFromEntity(StorageBox entity)
+    {
+        return new StorageBoxResource(
+            entity.Id,
+            entity.Label,
+            entity.Capacity.Max,
+            entity.Capacity.Current,
+            entity.StorageConditions.TemperatureRange,
+            entity.ProductInstances.Count);
+    }
+}

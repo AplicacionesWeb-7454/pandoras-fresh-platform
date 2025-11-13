fbdefb1e5de22271e0a9775a52bb13856399c7de
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:49:24 -0500

    feat(domain): add StorageConditions value object.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/ValueObjects/StorageConditions.cs
@@ -0,0 +1,6 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents storage conditions in the Pandora's Fresh Platform
+/// </summary>
+public record StorageConditions(string TemperatureRange);

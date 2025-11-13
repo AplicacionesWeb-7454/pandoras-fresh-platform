e2e4dc4f9b8c792dd2169d87b62ffb744f02d85d
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:26:41 -0500

    feat(events): add category created domain event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/CategoryCreatedEvent.cs
@@ -0,0 +1,8 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class CategoryCreatedEvent(string name) : IEvent
+{
+    public string Name { get; } = name;
+}

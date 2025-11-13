7c5bcc5a8f6eb62352d4448646b4357157e3ed9b
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:30:43 -0500

    feat(events): add product updated domain event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/ProductUpdatedEvent.cs
@@ -0,0 +1,12 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class ProductUpdatedEvent(ProductId productId, string name, string barcode, CategoryId categoryId) : IEvent
+{
+    public ProductId ProductId { get; } = productId;
+    public string Name { get; } = name;
+    public string Barcode { get; } = barcode;
+    public CategoryId CategoryId { get; } = categoryId;
+}

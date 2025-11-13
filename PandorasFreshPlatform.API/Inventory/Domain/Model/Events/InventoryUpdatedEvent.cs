7289763b35b2c4d48d379ec41d2cc96fc4941cb5
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:28:24 -0500

    feat(events): add inventory updated domain event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/InventoryUpdatedEvent.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class InventoryUpdatedEvent(InventoryItemId inventoryId, string name, string location) : IEvent
+{
+    public InventoryItemId InventoryId { get; } = inventoryId;
+    public string Name { get; } = name;
+    public string Location { get; } = location;
+}

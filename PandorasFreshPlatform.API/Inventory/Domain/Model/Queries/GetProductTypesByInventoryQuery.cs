580c9b70b064ecb2f877e972cc5fcc2537bfef4f
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:39:12 -0500

    feat(queries): add get product types by inventory query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetProductTypesByInventoryQuery.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get product types by inventory in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="InventoryId">
+/// The id of the inventory to get product types for
+/// </param>
+public record GetProductTypesByInventoryQuery(InventoryItemId InventoryId);

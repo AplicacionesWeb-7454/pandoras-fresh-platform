e57cb9d4d83ed42402c51a76b8fdd94e8daca6d2
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:57:57 -0500

    feat(inventory): add product instance to storage box resource.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Resources/AddProductInstanceToStorageBoxResource.cs
@@ -0,0 +1,18 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+
+/// <summary>
+/// Resource to add a product instance to a storage box
+/// </summary>
+/// <param name="ExpirationDate">
+/// The expiration date of the product instance
+/// </param>
+/// <param name="EntryDate">
+/// The entry date of the product instance
+/// </param>
+/// <param name="BatchNumber">
+/// The batch number of the product instance
+/// </param>
+/// <param name="ProductId">
+/// The ID of the product for the instance
+/// </param>
+public record AddProductInstanceToStorageBoxResource(DateTime ExpirationDate, DateTime EntryDate, string BatchNumber, int ProductId);

6b36d0ab3931f3353b5d190d929449ad1899aae1
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 16:00:14 -0500

    feat(inventory): add inventory item ID value object.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/ValueObjects/InventoryItemId.cs
@@ -0,0 +1,8 @@
+﻿// InventoryItemId.cs
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents the inventory item identifier in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="Id">The id of the inventory item.</param>
+public record InventoryItemId(int Id);

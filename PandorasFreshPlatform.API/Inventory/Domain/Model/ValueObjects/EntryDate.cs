0e07b9617e7312b3312f5c1e2d569ff9cdab66a9
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:43:54 -0500

    feat(domain): add EntryDate value object.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/ValueObjects/EntryDate.cs
@@ -0,0 +1,6 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents an entry date in the Pandora's Fresh Platform
+/// </summary>
+public record EntryDate(DateTime Date);

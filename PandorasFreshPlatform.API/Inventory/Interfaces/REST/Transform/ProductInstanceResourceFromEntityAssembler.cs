d684063bf08c31fc9525dde8462bd40ae63b7f6b
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:21:23 -0500

    feat(api): add assembler for product instance entity to resource conversion.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Transform/ProductInstanceResourceFromEntityAssembler.cs
@@ -0,0 +1,31 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.Entities;
+using PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+using Microsoft.OpenApi.Extensions;
+
+namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler class to convert ProductInstance to ProductInstanceResource
+/// </summary>
+public static class ProductInstanceResourceFromEntityAssembler
+{
+    /// <summary>
+    /// Convert ProductInstance to ProductInstanceResource
+    /// </summary>
+    /// <param name="entity">
+    /// The <see cref="ProductInstance" /> entity to convert
+    /// </param>
+    /// <returns>
+    /// The <see cref="ProductInstanceResource" /> resource
+    /// </returns>
+    public static ProductInstanceResource ToResourceFromEntity(ProductInstance entity)
+    {
+        return new ProductInstanceResource(
+            entity.Id,
+            entity.ExpirationDate.Date,
+            entity.EntryDate.Date,
+            entity.Batch.Number,
+            entity.Status.GetDisplayName(),
+            ProductResourceFromEntityAssembler.ToResourceFromEntity(entity.Product));
+    }
+}

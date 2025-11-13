9fc6e45366d449c3ac50c1520af5b456927e9bcd
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:20:17 -0500

    feat(scanning): add command to scan product barcodes.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/ScanBarcodeCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+/// <summary>
+/// Command to scan a barcode.
+/// </summary>
+/// <param name="Barcode">
+/// The barcode to scan.
+/// </param>
+/// <param name="DeviceId">
+/// The ID of the device performing the scan.
+/// </param>
+public record ScanBarcodeCommand(string Barcode, string DeviceId);

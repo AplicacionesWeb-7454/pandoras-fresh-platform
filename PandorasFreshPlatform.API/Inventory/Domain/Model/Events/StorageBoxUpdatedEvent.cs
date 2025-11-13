d77f4610593e55d3591dfeec00666cdc461f40fd
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:32:19 -0500

    feat(events): add storage box updated domain event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/StorageBoxUpdatedEvent.cs
@@ -0,0 +1,10 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class StorageBoxUpdatedEvent(StorageBoxId storageBoxId, string label) : IEvent
+{
+    public StorageBoxId StorageBoxId { get; } = storageBoxId;
+    public string Label { get; } = label;
+}

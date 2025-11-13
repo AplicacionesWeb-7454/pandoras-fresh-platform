720d8a990e798a74d7c82d4bd395dd4ca349224c
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:29:18 -0500

    feat(events): add ExpirationAlertEventHandler for product expiration notifications.

---
+++ PandorasFreshPlatform.API/Inventory/Application/Internal/EventHandlers/ExpirationAlertEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+using PandorasFreshPlatform.API.Shared.Application.Internal.EventHandlers;
+
+namespace PandorasFreshPlatform.API.Inventory.Application.Internal.EventHandlers;
+
+public class ExpirationAlertEventHandler : IEventHandler<ExpirationAlertEvent>
+{
+    public Task Handle(ExpirationAlertEvent domainEvent, CancellationToken cancellationToken)
+    {
+        return On(domainEvent);
+    }
+    
+    private static Task On(ExpirationAlertEvent domainEvent)
+    {
+        Console.WriteLine($"Expiration Alert: Product '{domainEvent.ProductName}' (ID: {domainEvent.ProductInstanceId}) " +
+                          $"expires on {domainEvent.ExpirationDate:yyyy-MM-dd} " +
+                          $"(in {domainEvent.DaysUntilExpiration} days)");
+        return Task.CompletedTask;
+    }
+}

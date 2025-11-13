16e50e5fb5a92223f5133cc24d4c600309d5a230
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:30:27 -0500

    feat(events): add product instance updated domain event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/ProductInstanceUpdatedEvent.cs
@@ -0,0 +1,10 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class ProductInstanceUpdatedEvent(ProductInstanceId productInstanceId, DateTime expirationDate) : IEvent
+{
+    public ProductInstanceId ProductInstanceId { get; } = productInstanceId;
+    public DateTime ExpirationDate { get; } = expirationDate;
+}

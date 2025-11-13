d5a0238ab29b9906b29c167b4b821d2d58cdd052
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:23:04 -0500

    feat(catalog): add Category entity for product classification.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Entities/Category.cs
@@ -0,0 +1,34 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Entities;
+
+/// <summary>
+/// Represents a category in the Pandora's Fresh Platform
+/// </summary>
+public class Category
+{
+    /// <summary>
+    /// Default constructor for the category entity
+    /// </summary>
+    public Category()
+    {
+        Name = string.Empty;
+    }
+
+    /// <summary>
+    /// Constructor for the category entity
+    /// </summary>
+    public Category(string name)
+    {
+        Name = name;
+    }
+
+    public Category(CreateCategoryCommand command)
+    {
+        Name = command.Name;
+    }
+
+    public int Id { get; set; }  // Own ID as primitive
+    public string Name { get; set; }
+    
+}

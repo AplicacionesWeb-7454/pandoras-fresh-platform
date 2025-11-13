f153fdfaeacd3ad9e262fb9c7ab18d1015bd74c5
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:36:05 -0500

    feat(queries): add get filtered product types query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetFilteredProductTypesQuery.cs
@@ -0,0 +1,14 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get filtered product types in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="CategoryId">
+/// The category id to filter by
+/// </param>
+/// <param name="InventoryId">
+/// The inventory id to filter by
+/// </param>
+public record GetFilteredProductTypesQuery(CategoryId? CategoryId, InventoryItemId? InventoryId);

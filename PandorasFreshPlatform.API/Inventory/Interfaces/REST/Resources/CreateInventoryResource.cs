b26540ae6940f085ddc16500fc891455621bb208
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:10:55 -0500

    feat(api): add CreateCategoryResource for category creation requests.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Resources/CreateInventoryResource.cs
@@ -0,0 +1,15 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+
+/// <summary>
+/// Create inventory resource for REST API
+/// </summary>
+/// <param name="Name">
+/// The name of the inventory
+/// </param>
+/// <param name="Description">
+/// The description of the inventory
+/// </param>
+/// <param name="Location">
+/// The location of the inventory
+/// </param>
+public record CreateInventoryResource(string Name, string Description, string Location);

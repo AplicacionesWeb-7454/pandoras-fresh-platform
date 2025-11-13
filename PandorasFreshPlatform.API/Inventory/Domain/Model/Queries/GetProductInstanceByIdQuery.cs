7eae10fc7b9240064fe6107a251bc6ea6399c793
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:38:28 -0500

    feat(queries): add get product instance by id query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetProductInstanceByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get a product instance by id in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="ProductInstanceId">
+/// The id of the product instance to get
+/// </param>
+public record GetProductInstanceByIdQuery(ProductInstanceId ProductInstanceId);

0ed8a0b9e10ae5bc2214f3d15276c6906f159308
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:40:43 -0500

    feat(queries): add get storage box with details query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetStorageBoxWithDetailsQuery.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get a storage box with detailed information in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="StorageBoxId">
+/// The id of the storage box to get with details
+/// </param>
+public record GetStorageBoxWithDetailsQuery(StorageBoxId StorageBoxId);

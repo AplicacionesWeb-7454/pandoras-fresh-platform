fe8df8d33c1919bcf4110cd3d6a3d00eb330116a
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:35:32 -0500

    feat(queries): add get filtered product instances query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetFilteredProductInstancesQuery.cs
@@ -0,0 +1,20 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get filtered product instances in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="CategoryId">
+/// The category id to filter by
+/// </param>
+/// <param name="Status">
+/// The status to filter by
+/// </param>
+/// <param name="InventoryId">
+/// The inventory id to filter by
+/// </param>
+/// <param name="StorageBoxId">
+/// The storage box id to filter by
+/// </param>
+public record GetFilteredProductInstancesQuery(CategoryId? CategoryId, string? Status, InventoryItemId? InventoryId, StorageBoxId? StorageBoxId);

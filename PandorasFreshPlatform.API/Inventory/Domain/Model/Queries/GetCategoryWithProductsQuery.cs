195f8826ec55c99dbd598168a28bf6dc427affcb
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:34:59 -0500

    feat(queries): add get category with products query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetCategoryWithProductsQuery.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get a category with its products in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="CategoryId">
+/// The id of the category to get with products
+/// </param>
+public record GetCategoryWithProductsQuery(CategoryId CategoryId);

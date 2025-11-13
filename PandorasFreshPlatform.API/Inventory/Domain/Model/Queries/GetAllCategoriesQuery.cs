3ea81e0dcaf920e237df0b98793354cf66e5d506
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:34:14 -0500

    feat(queries): add get all categories query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetAllCategoriesQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get all categories in the Pandora's Fresh Platform.
+/// </summary>
+public record GetAllCategoriesQuery;

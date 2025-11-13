986e44e5c1374ea7744ec26a569205445cfe9c37
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:24:57 -0500

    feat(storage): add StorageBoxIdentifier value object.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Entities/StorageBoxIdentifier.cs
@@ -0,0 +1,11 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Entities;
+
+/// <summary>
+/// Represents an identifier for a storage box in the Pandora's Fresh Platform
+/// </summary>
+public record StorageBoxIdentifier(Guid Identifier)
+{
+    public StorageBoxIdentifier() : this(Guid.NewGuid())
+    {
+    }
+}
---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/ValueObjects/StorageBoxId.cs
@@ -0,0 +1,8 @@
+﻿// StorageBoxId.cs
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents the storage box identifier in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="Id">The id of the storage box.</param>
+public record StorageBoxId(int Id);

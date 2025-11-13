8f48e4dcdeefe9cb399a21b84bfd48d19a7eafe1
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:32:03 -0500

    feat(events): add storage box deleted domain event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/StorageBoxDeletedEvent.cs
@@ -0,0 +1,9 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class StorageBoxDeletedEvent(StorageBoxId storageBoxId) : IEvent
+{
+    public StorageBoxId StorageBoxId { get; } = storageBoxId;
+}

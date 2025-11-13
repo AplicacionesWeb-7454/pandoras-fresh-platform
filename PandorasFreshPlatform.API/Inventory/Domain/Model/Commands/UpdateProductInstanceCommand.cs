acf0dfea2dfe3db88c4331c2ade0333ed964f87b
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:21:45 -0500

    feat(inventory): add command to update product instance information.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/UpdateProductInstanceCommand.cs
@@ -0,0 +1,21 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+/// <summary>
+/// Command to update a product instance.
+/// </summary>
+/// <param name="ProductInstanceId">
+/// The ID of the product instance to update.
+/// </param>
+/// <param name="ExpirationDate">
+/// The updated expiration date of the product instance.
+/// </param>
+/// <param name="BatchNumber">
+/// The updated batch number of the product instance.
+/// </param>
+public record UpdateProductInstanceCommand(
+    ProductInstanceId ProductInstanceId, 
+    DateTime ExpirationDate, 
+    string BatchNumber
+);

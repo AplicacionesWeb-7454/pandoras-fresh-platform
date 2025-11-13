6a235a445dc1d9941cca9051194ed6bc5252bc60
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:54:01 -0500

    feat(services): add inventory command service for inventory operations.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Services/IInventoryCommandService.cs
@@ -0,0 +1,43 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.Aggregates;
+using PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Services;
+
+/// <summary>
+/// Represents the inventory command service in the Pandora's Fresh Platform.
+/// </summary>
+public interface IInventoryCommandService
+{
+    /// <summary>
+    /// Handles the create inventory command in the Pandora's Fresh Platform.
+    /// </summary>
+    /// <param name="command">
+    /// The <see cref="CreateInventoryCommand" /> command to handle.
+    /// </param>
+    /// <returns>
+    /// The created <see cref="InventoryItem" /> entity.
+    /// </returns>
+    Task<InventoryItem?> Handle(CreateInventoryCommand command);
+
+    /// <summary>
+    /// Handles the update inventory command in the Pandora's Fresh Platform.
+    /// </summary>
+    /// <param name="command">
+    /// The <see cref="UpdateInventoryCommand" /> command to handle.
+    /// </param>
+    /// <returns>
+    /// The updated <see cref="InventoryItem" /> entity.
+    /// </returns>
+    Task<InventoryItem?> Handle(UpdateInventoryCommand command);
+
+    /// <summary>
+    /// Handles the delete inventory command in the Pandora's Fresh Platform.
+    /// </summary>
+    /// <param name="command">
+    /// The <see cref="DeleteInventoryCommand" /> command to handle.
+    /// </param>
+    /// <returns>
+    /// The result of the delete operation.
+    /// </returns>
+    Task<bool> Handle(DeleteInventoryCommand command);
+}

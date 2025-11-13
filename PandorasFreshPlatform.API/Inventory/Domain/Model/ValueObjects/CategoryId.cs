afe4ef73b6f27a4922f442508e5843ba79baf806
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:43:37 -0500

    feat(domain): add CategoryId value object.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/ValueObjects/CategoryId.cs
@@ -0,0 +1,7 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents a category identifier in the Pandora's Fresh Platform
+/// </summary>
+/// <param name="Value">The integer value of the category ID</param>
+public record CategoryId(int Value);

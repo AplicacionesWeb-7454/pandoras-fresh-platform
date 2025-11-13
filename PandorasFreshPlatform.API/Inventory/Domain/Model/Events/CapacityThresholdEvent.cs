3c787f0fcf9f260a4d9af3a0c2dcfde99958d599
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:26:29 -0500

    feat(events): add storage box capacity threshold event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/CapacityThresholdEvent.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class CapacityThresholdEvent(int storageBoxId, string label, int currentCapacity, int maxCapacity) : IEvent
+{
+    public int StorageBoxId { get; } = storageBoxId;
+    public string Label { get; } = label;
+    public int CurrentCapacity { get; } = currentCapacity;
+    public int MaxCapacity { get; } = maxCapacity;
+}

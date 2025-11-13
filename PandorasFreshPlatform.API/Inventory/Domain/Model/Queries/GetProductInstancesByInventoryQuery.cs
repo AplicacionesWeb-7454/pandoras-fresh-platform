6e87b5c73bde825172b4b64d78f254dc32e6742c
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:38:38 -0500

    feat(queries): add get product instances by inventory query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetProductInstancesByInventoryQuery.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get product instances by inventory in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="InventoryId">
+/// The id of the inventory to get product instances for
+/// </param>
+public record GetProductInstancesByInventoryQuery(InventoryItemId InventoryId);

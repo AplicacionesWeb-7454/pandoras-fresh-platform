7a87483f95f46a2573c02e1f66b0384fe6e6e2f6
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:12:16 -0500

    feat(api): add ProductInstanceResource with product details for API responses.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Resources/ProductInstanceResource.cs
@@ -0,0 +1,24 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+
+/// <summary>
+/// Product instance resource for REST API
+/// </summary>
+/// <param name="Id">
+/// The unique identifier of the product instance
+/// </param>
+/// <param name="ExpirationDate">
+/// The expiration date of the product instance
+/// </param>
+/// <param name="EntryDate">
+/// The entry date of the product instance
+/// </param>
+/// <param name="BatchNumber">
+/// The batch number of the product instance
+/// </param>
+/// <param name="Status">
+/// The status of the product instance
+/// </param>
+/// <param name="Product">
+/// The product of the product instance
+/// </param>
+public record ProductInstanceResource(int Id, DateTime ExpirationDate, DateTime EntryDate, string BatchNumber, string Status, ProductResource Product);

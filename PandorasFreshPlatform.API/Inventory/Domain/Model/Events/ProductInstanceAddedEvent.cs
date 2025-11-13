fd8ac58912248ec36fe2536b28031790661fbf69
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:29:31 -0500

    feat(events): add product instance added to storage event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/ProductInstanceAddedEvent.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class ProductInstanceAddedEvent(ProductId productId, StorageBoxId storageBoxId, DateTime expirationDate) : IEvent
+{
+    public ProductId ProductId { get; } = productId;
+    public StorageBoxId StorageBoxId { get; } = storageBoxId;
+    public DateTime ExpirationDate { get; } = expirationDate;
+}

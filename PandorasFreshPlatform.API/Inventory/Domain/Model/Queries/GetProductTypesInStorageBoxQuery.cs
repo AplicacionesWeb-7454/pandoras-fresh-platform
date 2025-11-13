1f4271bd4736cc6ff6a2ca2619379448c831d18a
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:39:20 -0500

    feat(queries): add get product types in storage box query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetProductTypesInStorageBoxQuery.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get product types in a storage box in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="StorageBoxId">
+/// The id of the storage box to get product types for
+/// </param>
+public record GetProductTypesInStorageBoxQuery(StorageBoxId StorageBoxId);

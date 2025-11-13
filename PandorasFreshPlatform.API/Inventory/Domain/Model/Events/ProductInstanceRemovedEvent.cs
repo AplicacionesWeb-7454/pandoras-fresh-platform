19c82bb2d16a528a3d5a4e188af6e1dd9f28d888
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:29:47 -0500

    feat(events): add product instance removed domain event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/ProductInstanceRemovedEvent.cs
@@ -0,0 +1,9 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class ProductInstanceRemovedEvent(ProductInstanceId productInstanceId) : IEvent
+{
+    public ProductInstanceId ProductInstanceId { get; } = productInstanceId;
+}

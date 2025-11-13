903391f33ed0a7c9455ae7363dd4618f661f6f57
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:16:33 -0500

    feat(inventory): add command to place product instances in storage boxes.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/AddProductInstanceToStorageBoxCommand.cs
@@ -0,0 +1,29 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+/// <summary>
+/// Command to add a product instance to a storage box.
+/// </summary>
+/// <param name="ExpirationDate">
+/// The expiration date of the product instance.
+/// </param>
+/// <param name="EntryDate">
+/// The entry date of the product instance.
+/// </param>
+/// <param name="BatchNumber">
+/// The batch number of the product instance.
+/// </param>
+/// <param name="ProductId">
+/// The ID of the product for the instance.
+/// </param>
+/// <param name="StorageBoxId">
+/// The ID of the storage box to add the product instance to.
+/// </param>
+public record AddProductInstanceToStorageBoxCommand(
+    DateTime ExpirationDate, 
+    DateTime EntryDate, 
+    string BatchNumber, 
+    ProductId ProductId, 
+    StorageBoxId StorageBoxId
+);

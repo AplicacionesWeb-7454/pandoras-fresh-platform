61a4f158211241927c9c438d1dc9e2ceb4d23af6
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:18:37 -0500

    feat(inventory): add command to delete inventory locations.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/DeleteInventoryCommand.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+/// <summary>
+/// Command to delete an inventory.
+/// </summary>
+/// <param name="InventoryId">
+/// The ID of the inventory to delete.
+/// </param>
+public record DeleteInventoryCommand(InventoryItemId InventoryId);

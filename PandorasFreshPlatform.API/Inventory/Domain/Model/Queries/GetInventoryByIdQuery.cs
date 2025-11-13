eb2aa501fc265e3f079fc071e9cf56c7d87f53ba
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:36:27 -0500

    feat(queries): add get inventory by id query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetInventoryByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get an inventory by id in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="InventoryId">
+/// The id of the inventory to get
+/// </param>
+public record GetInventoryByIdQuery(InventoryItemId InventoryId);

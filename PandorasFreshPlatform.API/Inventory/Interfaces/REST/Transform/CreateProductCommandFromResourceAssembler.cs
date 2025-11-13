e3693b04438aa516b99f791e3e0ae26e5ceec14b
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:18:33 -0500

    feat(api): add assembler for product creation command conversion.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Transform/CreateProductCommandFromResourceAssembler.cs
@@ -0,0 +1,29 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+using PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+
+namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler class to convert CreateProductResource to CreateProductCommand
+/// </summary>
+public static class CreateProductCommandFromResourceAssembler
+{
+    /// <summary>
+    /// Convert CreateProductResource to CreateProductCommand
+    /// </summary>
+    /// <param name="resource">
+    /// The <see cref="CreateProductResource" /> resource
+    /// </param>
+    /// <returns>
+    /// The <see cref="CreateProductCommand" /> command
+    /// </returns>
+    public static CreateProductCommand ToCommandFromResource(CreateProductResource resource)
+    {
+        return new CreateProductCommand(
+            resource.Name, 
+            resource.Description, 
+            resource.Barcode, 
+            new CategoryId(resource.CategoryId));
+    }
+}

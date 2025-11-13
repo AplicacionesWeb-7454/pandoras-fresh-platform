31cb1affd03219844a223307e403c35d500efee1
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:29:57 -0500

    feat(events): add ProductCreatedEventHandler for product creation logging.

---
+++ PandorasFreshPlatform.API/Inventory/Application/Internal/EventHandlers/ProductCreatedEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+using PandorasFreshPlatform.API.Shared.Application.Internal.EventHandlers;
+
+namespace PandorasFreshPlatform.API.Inventory.Application.Internal.EventHandlers;
+
+public class ProductCreatedEventHandler : IEventHandler<ProductCreatedEvent>
+{
+    public Task Handle(ProductCreatedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        return On(domainEvent);
+    }
+    
+    private static Task On(ProductCreatedEvent domainEvent)
+    {
+        Console.WriteLine($"Created Product: {domainEvent.Name} with Barcode: {domainEvent.Barcode} in Category: {domainEvent.CategoryId}");
+        return Task.CompletedTask;
+    }
+}

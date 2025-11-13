7bc684afea068fd7cdf046cc2c662c41a80cef8e
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:38:01 -0500

    feat(queries): add get product by id query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get a product by id in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="ProductId">
+/// The id of the product to get
+/// </param>
+public record GetProductByIdQuery(ProductId ProductId);

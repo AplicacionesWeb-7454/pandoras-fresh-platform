46677d72260855955e9a83e1a9150f68cd1c706f
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:29:37 -0500

    feat(events): add InventoryCreatedEventHandler for inventory creation logging.

---
+++ PandorasFreshPlatform.API/Inventory/Application/Internal/EventHandlers/InventoryCreatedEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+using PandorasFreshPlatform.API.Shared.Application.Internal.EventHandlers;
+
+namespace PandorasFreshPlatform.API.Inventory.Application.Internal.EventHandlers;
+
+public class InventoryCreatedEventHandler : IEventHandler<InventoryCreatedEvent>
+{
+    public Task Handle(InventoryCreatedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        return On(domainEvent);
+    }
+    
+    private static Task On(InventoryCreatedEvent domainEvent)
+    {
+        Console.WriteLine($"Created Inventory: {domainEvent.Name} at Location: {domainEvent.Location}");
+        return Task.CompletedTask;
+    }
+}

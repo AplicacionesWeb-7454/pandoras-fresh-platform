35a2534d5604008175753970ad8d06544f9f5478
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:16:47 -0500

    feat(inventory): add batch operation for multiple product instances.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/BatchAddProductInstancesCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+/// <summary>
+/// Command to add multiple product instances in a batch operation.
+/// </summary>
+/// <param name="ProductInstances">
+/// Collection of product instances to add.
+/// </param>
+public record BatchAddProductInstancesCommand(IEnumerable<AddProductInstanceToStorageBoxCommand> ProductInstances);

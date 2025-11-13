ef4ac26f150140ac6a6c11de5c6b3955361cccb4
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:18:52 -0500

    feat(catalog): add command to delete products.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/DeleteProductCommand.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+/// <summary>
+/// Command to delete a product.
+/// </summary>
+/// <param name="ProductId">
+/// The ID of the product to delete.
+/// </param>
+public record DeleteProductCommand(ProductId ProductId);

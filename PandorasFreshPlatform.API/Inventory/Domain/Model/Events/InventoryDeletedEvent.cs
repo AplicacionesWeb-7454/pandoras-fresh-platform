56b825b6828219b7fb0a51a2477d52d720bd06b6
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:27:38 -0500

    feat(events): add inventory deleted domain event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/InventoryDeletedEvent.cs
@@ -0,0 +1,9 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class InventoryDeletedEvent(InventoryItemId inventoryId) : IEvent
+{
+    public InventoryItemId InventoryId { get; } = inventoryId;
+}

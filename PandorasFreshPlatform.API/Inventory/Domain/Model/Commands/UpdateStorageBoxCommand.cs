209b463005945f99fbc646369b8afba5071ded11
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:22:04 -0500

    feat(storage): add command to update storage box properties.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/UpdateStorageBoxCommand.cs
@@ -0,0 +1,29 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+/// <summary>
+/// Command to update a storage box.
+/// </summary>
+/// <param name="StorageBoxId">
+/// The ID of the storage box to update.
+/// </param>
+/// <param name="Label">
+/// The updated label of the storage box.
+/// </param>
+/// <param name="MaxCapacity">
+/// The updated maximum capacity of the storage box.
+/// </param>
+/// <param name="CurrentCapacity">
+/// The updated current capacity of the storage box.
+/// </param>
+/// <param name="TemperatureRange">
+/// The updated temperature range for the storage box.
+/// </param>
+public record UpdateStorageBoxCommand(
+    StorageBoxId StorageBoxId, 
+    string Label, 
+    int MaxCapacity, 
+    int CurrentCapacity, 
+    string TemperatureRange
+);

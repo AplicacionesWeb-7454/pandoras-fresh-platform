8b0195e88b992334e3cd2034a81b6ef78f98071c
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:14:08 -0500

    feat(audit): add audit fields to InventoryItem entity.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Aggregates/InventoryItemAudit.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Aggregates;
+
+public partial class InventoryItem : IEntityWithCreatedUpdatedDate
+{
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}

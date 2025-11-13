be5fceddbd4783b142128c296378f61daf1b6e50
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:21:52 -0500

    feat(api): add assembler for barcode scanning command conversion.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Transform/ScanBarcodeCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+using PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+
+namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Transform;
+
+public static class ScanBarcodeCommandFromResourceAssembler
+{
+    public static ScanBarcodeCommand ToCommandFromResource(ScanBarcodeResource resource)
+    {
+        return new ScanBarcodeCommand(resource.Barcode, resource.DeviceId);
+    }
+}

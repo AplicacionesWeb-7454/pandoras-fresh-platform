cb10d23168d8e800651adadfe223c055f54d3fb4
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:17:59 -0500

    feat(api): add assembler for inventory creation command conversion.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Transform/CreateInventoryCommandFromResourceAssembler.cs
@@ -0,0 +1,24 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+using PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+
+namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler class to convert CreateInventoryResource to CreateInventoryCommand
+/// </summary>
+public static class CreateInventoryCommandFromResourceAssembler
+{
+    /// <summary>
+    /// Convert CreateInventoryResource to CreateInventoryCommand
+    /// </summary>
+    /// <param name="resource">
+    /// The <see cref="CreateInventoryResource" /> resource
+    /// </param>
+    /// <returns>
+    /// The <see cref="CreateInventoryCommand" /> command
+    /// </returns>
+    public static CreateInventoryCommand ToCommandFromResource(CreateInventoryResource resource)
+    {
+        return new CreateInventoryCommand(resource.Name, resource.Description, resource.Location);
+    }
+}

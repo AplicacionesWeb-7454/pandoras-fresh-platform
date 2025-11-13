31cf91668b1d9f774288583dbd29e863fec9ea50
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:43:26 -0500

    feat(domain): add Capacity value object for storage management.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/ValueObjects/Capacity.cs
@@ -0,0 +1,6 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents capacity information in the Pandora's Fresh Platform
+/// </summary>
+public record Capacity(int Max, int Current);

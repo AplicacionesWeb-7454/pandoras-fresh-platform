a673e6a94b2f08623d38e3e72bea69cb7abd42fe
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:27:05 -0500

    feat(events): add product expiration alert event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/ExpirationAlertEvent.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class ExpirationAlertEvent(int productInstanceId, string productName, DateTime expirationDate, int daysUntilExpiration) : IEvent
+{
+    public int ProductInstanceId { get; } = productInstanceId;
+    public string ProductName { get; } = productName;
+    public DateTime ExpirationDate { get; } = expirationDate;
+    public int DaysUntilExpiration { get; } = daysUntilExpiration;
+}

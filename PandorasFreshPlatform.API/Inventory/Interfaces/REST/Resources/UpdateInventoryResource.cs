74a5a35fb921359ecbe4bb139b9fb4d3ad67ae77
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:13:53 -0500

    feat(api): add UpdateInventoryResource for inventory update requests.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Resources/UpdateInventoryResource.cs
@@ -0,0 +1,15 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+
+/// <summary>
+/// Update inventory resource for REST API
+/// </summary>
+/// <param name="Name">
+/// The name of the inventory
+/// </param>
+/// <param name="Description">
+/// The description of the inventory
+/// </param>
+/// <param name="Location">
+/// The location of the inventory
+/// </param>
+public record UpdateInventoryResource(string Name, string Description, string Location);

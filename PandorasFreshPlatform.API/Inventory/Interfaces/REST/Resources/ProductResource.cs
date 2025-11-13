d655f948e763f539b86e069ed47e23f573de3a76
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:12:53 -0500

    feat(api): add ProductResource with category details for API responses.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Resources/ProductResource.cs
@@ -0,0 +1,21 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+
+/// <summary>
+/// Product resource for REST API
+/// </summary>
+/// <param name="Id">
+/// The unique identifier of the product
+/// </param>
+/// <param name="Name">
+/// The name of the product
+/// </param>
+/// <param name="Description">
+/// The description of the product
+/// </param>
+/// <param name="Barcode">
+/// The barcode of the product
+/// </param>
+/// <param name="Category">
+/// The category of the product
+/// </param>
+public record ProductResource(int Id, string Name, string Description, string Barcode, CategoryResource Category);

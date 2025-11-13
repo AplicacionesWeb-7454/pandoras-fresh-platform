9edc5323fc5413339f83c6665122e29f7b2921b7
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:11:56 -0500

    feat(api): add InventoryResource with storage box count for API responses.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Resources/InventoryResource.cs
@@ -0,0 +1,21 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+
+/// <summary>
+/// Inventory resource for REST API
+/// </summary>
+/// <param name="Id">
+/// The unique identifier of the inventory
+/// </param>
+/// <param name="Name">
+/// The name of the inventory
+/// </param>
+/// <param name="Description">
+/// The description of the inventory
+/// </param>
+/// <param name="Location">
+/// The location of the inventory
+/// </param>
+/// <param name="StorageBoxCount">
+/// The number of storage boxes in the inventory
+/// </param>
+public record InventoryResource(int Id, string Name, string Description, string Location, int StorageBoxCount);

cebbc1b28776780ee45834c1393b9e6d6d90fb5e
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:24:13 -0500

    feat(inventory): add ProductInstanceIdentifier value object.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Entities/ProductInstanceIdentifier.cs
@@ -0,0 +1,11 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Entities;
+
+/// <summary>
+/// Represents an identifier for a product instance in the Pandora's Fresh Platform
+/// </summary>
+public record ProductInstanceIdentifier(Guid Identifier)
+{
+    public ProductInstanceIdentifier() : this(Guid.NewGuid())
+    {
+    }
+}
---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/ValueObjects/ProductInstanceId.cs
@@ -0,0 +1,8 @@
+﻿// ProductInstanceId.cs
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents the product instance identifier in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="Id">The id of the product instance.</param>
+public record ProductInstanceId(int Id);

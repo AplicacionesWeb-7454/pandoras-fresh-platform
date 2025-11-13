8bd9a9e05e2d18351c167a6789948d842860598a
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:27:26 -0500

    feat(events): add inventory created domain event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/InventoryCreatedEvent.cs
@@ -0,0 +1,9 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class InventoryCreatedEvent(string name, string location) : IEvent
+{
+    public string Name { get; } = name;
+    public string Location { get; } = location;
+}

ab8d7f54c13761564d71c0aad05a2dc4d1d36911
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:19:54 -0500

    feat(inventory): add command to remove product instances.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/RemoveProductInstanceCommand.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+/// <summary>
+/// Command to remove a product instance.
+/// </summary>
+/// <param name="ProductInstanceId">
+/// The ID of the product instance to remove.
+/// </param>
+public record RemoveProductInstanceCommand(ProductInstanceId ProductInstanceId);

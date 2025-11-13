7354eceb8db24aaf9b4e994a1b2dd3991c9ff4f3
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:44:39 -0500

    feat(domain): add ExpirationDate value object.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/ValueObjects/ExpirationDate.cs
@@ -0,0 +1,6 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents an expiration date in the Pandora's Fresh Platform
+/// </summary>
+public record ExpirationDate(DateTime Date);

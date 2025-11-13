8b9fac4c7cc2b73747f2e4c3e3babefe8745d296
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:37:20 -0500

    feat(queries): add get inventory summary query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetInventorySummaryQuery.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get an inventory summary in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="InventoryId">
+/// The id of the inventory to get the summary for
+/// </param>
+public record GetInventorySummaryQuery(InventoryItemId InventoryId);

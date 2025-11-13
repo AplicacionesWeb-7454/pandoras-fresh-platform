8afc877b8ce86db0f624a4b8d409ff1a6e56aaff
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:37:47 -0500

    feat(queries): add get product by barcode query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetProductByBarcodeQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get a product by barcode in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="Barcode">
+/// The barcode of the product to get
+/// </param>
+public record GetProductByBarcodeQuery(string Barcode);

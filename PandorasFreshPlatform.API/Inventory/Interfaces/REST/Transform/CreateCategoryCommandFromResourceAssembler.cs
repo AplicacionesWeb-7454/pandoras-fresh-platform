ffbaf00859c3af356e4b3b6a58420a32abfc52a1
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:17:42 -0500

    feat(api): add assembler for category creation command conversion.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Transform/CreateCategoryCommandFromResourceAssembler.cs
@@ -0,0 +1,24 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+using PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+
+namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler class to convert CreateCategoryResource to CreateCategoryCommand
+/// </summary>
+public static class CreateCategoryCommandFromResourceAssembler
+{
+    /// <summary>
+    /// Convert CreateCategoryResource to CreateCategoryCommand
+    /// </summary>
+    /// <param name="resource">
+    /// The <see cref="CreateCategoryResource" /> resource
+    /// </param>
+    /// <returns>
+    /// The <see cref="CreateCategoryCommand" /> command
+    /// </returns>
+    public static CreateCategoryCommand ToCommandFromResource(CreateCategoryResource resource)
+    {
+        return new CreateCategoryCommand(resource.Name);
+    }
+}

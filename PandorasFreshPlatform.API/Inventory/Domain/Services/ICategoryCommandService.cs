81a81e823c96af68a96217b81027379b81d2e3b1
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:53:38 -0500

    feat(services): add category command service for category management.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Services/ICategoryCommandService.cs
@@ -0,0 +1,21 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.Entities;
+using PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Services;
+
+/// <summary>
+/// Represents the category command service in the Pandora's Fresh Platform.
+/// </summary>
+public interface ICategoryCommandService
+{
+    /// <summary>
+    /// Handles the create category command in the Pandora's Fresh Platform.
+    /// </summary>
+    /// <param name="command">
+    /// The <see cref="CreateCategoryCommand" /> command to handle.
+    /// </param>
+    /// <returns>
+    /// The created <see cref="Category" /> entity.
+    /// </returns>
+    Task<Category?> Handle(CreateCategoryCommand command);
+}

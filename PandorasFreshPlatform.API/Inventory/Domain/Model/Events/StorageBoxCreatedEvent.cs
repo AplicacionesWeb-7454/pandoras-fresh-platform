9bf042ca435e649ac33a915dfd114aa35805d964
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:30:57 -0500

    feat(events): add storage box created domain event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/StorageBoxCreatedEvent.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class StorageBoxCreatedEvent(string label, InventoryItemId inventoryId) : IEvent
+{
+    public string Label { get; } = label;
+    public InventoryItemId InventoryId { get; } = inventoryId;
+}

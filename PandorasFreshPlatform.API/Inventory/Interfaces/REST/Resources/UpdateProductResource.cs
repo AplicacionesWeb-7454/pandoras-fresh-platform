0cfeea198f4c47b623563f41088d711d1c20b39d
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 15:14:07 -0500

    feat(api): add UpdateProductResource for product update requests.

---
+++ PandorasFreshPlatform.API/Inventory/Interfaces/REST/Resources/UpdateProductResource.cs
@@ -0,0 +1,18 @@
+﻿namespace PandorasFreshPlatform.API.Inventory.Interfaces.REST.Resources;
+
+/// <summary>
+/// Update product resource for REST API
+/// </summary>
+/// <param name="Name">
+/// The name of the product
+/// </param>
+/// <param name="Description">
+/// The description of the product
+/// </param>
+/// <param name="Barcode">
+/// The barcode of the product
+/// </param>
+/// <param name="CategoryId">
+/// The unique identifier of the category
+/// </param>
+public record UpdateProductResource(string Name, string Description, string Barcode, int CategoryId);

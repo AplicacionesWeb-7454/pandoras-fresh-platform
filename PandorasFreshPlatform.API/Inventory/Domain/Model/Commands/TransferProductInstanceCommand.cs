6736c64b0e54585204d195820d75a468b8b9b6a3
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:20:36 -0500

    feat(inventory): add command to transfer product instances between boxes.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Commands/TransferProductInstanceCommand.cs
@@ -0,0 +1,21 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Commands;
+
+/// <summary>
+/// Command to transfer a product instance between storage boxes.
+/// </summary>
+/// <param name="ProductInstanceId">
+/// The ID of the product instance to transfer.
+/// </param>
+/// <param name="FromStorageBoxId">
+/// The ID of the source storage box.
+/// </param>
+/// <param name="ToStorageBoxId">
+/// The ID of the destination storage box.
+/// </param>
+public record TransferProductInstanceCommand(
+    ProductInstanceId ProductInstanceId, 
+    StorageBoxId FromStorageBoxId, 
+    StorageBoxId ToStorageBoxId
+);

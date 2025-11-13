92f076f81ee6b1d541c66cb022b0ae522bcfcfc3
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:40:32 -0500

    feat(queries): add get storage boxes by inventory query.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Queries/GetStorageBoxesByInventoryQuery.cs
@@ -0,0 +1,11 @@
+﻿using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get storage boxes by inventory in the Pandora's Fresh Platform.
+/// </summary>
+/// <param name="InventoryId">
+/// The id of the inventory to get storage boxes for
+/// </param>
+public record GetStorageBoxesByInventoryQuery(InventoryItemId InventoryId);

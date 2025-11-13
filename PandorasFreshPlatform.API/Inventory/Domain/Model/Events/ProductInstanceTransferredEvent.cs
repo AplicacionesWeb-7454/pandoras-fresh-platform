4a1ed77301a4a3a5e0478bd9230f2914e0e0d59d
DiegoLlamozas <diegollamozas911@gmail.com>
2025-11-13 11:30:06 -0500

    feat(events): add product instance transferred between boxes event.

---
+++ PandorasFreshPlatform.API/Inventory/Domain/Model/Events/ProductInstanceTransferredEvent.cs
@@ -0,0 +1,12 @@
+﻿using PandorasFreshPlatform.API.Shared.Domain.Model.Events;
+using PandorasFreshPlatform.API.Inventory.Domain.Model.ValueObjects;
+
+
+namespace PandorasFreshPlatform.API.Inventory.Domain.Model.Events;
+
+public class ProductInstanceTransferredEvent(ProductInstanceId productInstanceId, StorageBoxId fromStorageBoxId, StorageBoxId toStorageBoxId) : IEvent
+{
+    public ProductInstanceId ProductInstanceId { get; } = productInstanceId;
+    public StorageBoxId FromStorageBoxId { get; } = fromStorageBoxId;
+    public StorageBoxId ToStorageBoxId { get; } = toStorageBoxId;
+}
